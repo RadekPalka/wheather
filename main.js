b953ffe15714ade2211d64c6ec1de2e2a2c8863a
Radoslaw Palka <radekpalka@gmail..com>
2022-05-22 19:11:01 +0200

    add app

---
+++ main.js
@@ -0,0 +1,33 @@
+const selectElement= document.querySelector("select");
+const intro = document.querySelector("p.intro")
+const dateParagraph = document.querySelector("p.date");
+const tempMessageParagraph = document.querySelector("p.temp-message");
+const rainMessageParagraph = document.querySelector("p.rain-message")
+
+let jData;
+(async() =>{
+    const data= await fetch("https://danepubliczne.imgw.pl/api/data/synop")
+    jData= await data.json()
+    console.log(jData)
+    jData.forEach(el =>{
+        const optionElement= document.createElement("option")
+        optionElement.setAttribute("value" , `${el.stacja}`)
+        optionElement.textContent = el.stacja
+        selectElement.appendChild(optionElement)
+    })
+})()
+
+const displayCityData= city =>{
+    if (city !== "Wybierz miasto") {
+        console.log(city)
+        const result = jData.filter(el => el.stacja === city)
+        const {stacja, data_pomiaru, godzina_pomiaru, temperatura, suma_opadu} = result[0]
+        intro.textContent = `Wyświetlam pogodę dla miasta ${stacja}`
+        dateParagraph.textContent = `Data i godzina pomiaru: ${data_pomiaru}  ${godzina_pomiaru}:00`
+        tempMessageParagraph.textContent = `Temperatura: ${temperatura} °C`
+        rainMessageParagraph.textContent = `Suma opadu: ${suma_opadu}`
+    }
+
+}
+
+selectElement.addEventListener("click", (e)=> displayCityData(e.target.value))
